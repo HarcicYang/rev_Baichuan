7d21279f6e6001a2b2704956c6789178376c83b7
Harcic <harcic@outlook.com>
2023-12-03 12:38:52 +0800

    oi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rev_baichuan",
-    version="0.1",
+    version="0.11",
     description="通过cookie信息快速访问百川智能大语言模型",
     author="Harcic",
     author_email="harcic@outlook.com",
